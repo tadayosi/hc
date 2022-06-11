f4605bc4c2421c74ede45e7bac260b1824c9beaa
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-06-12 00:00:38 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  EmptyState, EmptyStateIcon, EmptyStateVariant,
+  Nav, NavItem, NavList,
+  Page, PageHeader, PageSection, PageSectionVariants, PageSidebar,
+  Title
+} from '@patternfly/react-core';
+import '@patternfly/react-core/dist/styles/base.css';
+import { CubesIcon } from '@patternfly/react-icons';
+import * as React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello!
-        </p>
-      </header>
-    </div>
-  );
-}
+type AppProps = {};
+
+const App: React.FunctionComponent<AppProps> = () => (
+  <BrowserRouter>
+    <Page
+      header={<AppHeader />}
+      sidebar={<AppSidebar />}
+      isManagedSidebar>
+      <Switch>
+        <Route path='/app1' />
+        <Route path='/app2' />
+        <Route path='/app3' />
+        <Route path='/'>
+          <PageSection variant={PageSectionVariants.light}>
+            <EmptyState variant={EmptyStateVariant.full}>
+              <EmptyStateIcon icon={CubesIcon} />
+              <Title headingLevel="h5" size="lg">Hello!</Title>
+            </EmptyState>
+          </PageSection>
+        </Route>
+      </Switch>
+    </Page>
+  </BrowserRouter>
+);
+
+const AppHeader: React.FunctionComponent = () => (
+  <React.Fragment>
+    <PageHeader
+      showNavToggle
+    />
+  </React.Fragment>
+);
+
+const AppSidebar: React.FunctionComponent = () => (
+  <PageSidebar theme="dark">
+    <Nav theme="dark">
+      <NavList>
+        <NavItem itemId={0}>
+          <Link to='/app1'>App 1</Link>
+        </NavItem>
+        <NavItem itemId={1}>
+          <Link to='/app2'>App 2</Link>
+        </NavItem>
+        <NavItem itemId={2}>
+          <Link to='/app3'>App 3</Link>
+        </NavItem>
+      </NavList>
+    </Nav>
+  </PageSidebar>
+);
 
 export default App;

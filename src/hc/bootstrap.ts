f60d1322dec417e78fb2f219175fda694f8aef04
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-06-12 21:08:55 +0900

    chore

--- src/hc/bootstrap.ts
+++ src/hc/bootstrap.ts
@@ -2,7 +2,7 @@ import pluginLoader from './plugin-loader';
 
 const bootstrap = () => {
   pluginLoader.loadPlugins(plugins => {
-    console.log('Loaded external plugins:', plugins);
+    console.log('Loaded plugins:', plugins);
   });
   console.log('Bootstrapped HC');
 };
